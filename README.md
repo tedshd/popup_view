59248560d7ba59e5ded50db9fa092214f8a8a892
tedshd <tedshd@gmail.com>
2015-07-23 18:56:38 +0800

    [Dev] Update code.

--- README.md
+++ README.md
@@ -1,3 +1,58 @@
 # popup_view
 popup view only use javascript
 
+## Intro
+
+This is popup view.
+
+## Feature
+
+* Use CSS3
+* Only JavaScript
+* Only support IE10 up
+
+## Method
+
+* show(dom, show, hide)
+  * dom[object] - popup you want show
+  * show[function] - do something went popup show
+  * hide[function] - do something went popup hide
+
+* hide(dom, hide)
+  * dom[object] - popup you want hide
+  * hide[function] - do something went hide
+
+## Usage
+
+### HTML
+
+```html
+<!-- basic popup view structure -->
+<div class="pop_up popup_hide">
+    <div class="pop_up_close"></div>
+</div>
+```
+
+### JavaScript
+
+```javascript
+var popupView = new popup();
+
+popupView.show(document.querySelector('#popup_1'));
+```
+
+```javascript
+var popupView = new popup();
+
+popupView.show(document.querySelector('#popup_2'), function () {
+    console.log('show do something');
+});
+```
+
+```javascript
+var popupView = new popup();
+
+popupView.show(document.querySelector('#popup_3'), '', function () {
+    console.log('CLOSE');
+});
+```
--- javascript/popup_view.js
+++ javascript/popup_view.js
@@ -5,58 +5,38 @@
  */
 
 (function () {
-    var popup = function(option) {
-        var initDom;
-
-        if (option && Object.keys(option).length) {
-            var w = option.width || '100%',
-                h = option.height || '100%',
-                top = option.top || '0',
-                left = option.left || '0',
-                marginTop = option.marginTop || '0',
-                marginLeft = option.marginLeft || '0';
-            if (!option.dom) {
-                console.error('not set dom parameters when you new popup()');
-                return;
-            }
-            initDom = option.dom;
-            initDom.style.width = w;
-            initDom.style.height = h;
-            initDom.style.top = top;
-            initDom.style.left = left;
-            initDom.style.marginTop = marginTop;
-            initDom.style.marginLeft = marginLeft;
-        }
+    var popup = function() {
         function hide(dom, dosomething) {
-            if (!initDom && !dom) {
+            if (!dom) {
                 console.error('hide function not set dom object');
                 return;
             }
             if (dosomething) {
                 dosomething();
             }
-            var currentDom = dom || initDom;
-            currentDom.className += ' ' + 'popup_hide';
+            dom.className += ' ' + 'popup_hide';
         }
-        function show(dom, dosomething) {
-            if (!initDom && !dom) {
+        function show(dom, dosomethingShow, dosomethingClose) {
+            if (!dom) {
                 console.error('show function not set dom object');
                 return;
             }
-            if (dosomething) {
-                dosomething();
+            if (dosomethingShow) {
+                dosomethingShow();
             }
-            var currentDom = dom || initDom;
             var className = 'popup_hide',
                 reg = new RegExp('(^|\\b)' +
                     className.split(' ').join('|') +
                     '(\\b|$)', 'gi');
-            currentDom.className = currentDom.className.replace(reg, '').trim();
-            var nodes = currentDom.childNodes;
+            dom.className = dom.className.replace(reg, '').trim();
+            var nodes = dom.childNodes;
             for (var i = nodes.length - 1; i >= 0; i--) {
                 if (nodes[i].className === 'pop_up_close') {
                     var close = function (e) {
-                        currentDom.className += ' ' + 'popup_hide';
+                        if (dosomethingClose) {
+                            dosomethingClose();
+                        }
+                        dom.className += ' ' + 'popup_hide';
                         nodes[i].removeEventListener('click', close);
                     };
                     nodes[i].addEventListener('click', close);
@@ -65,6 +45,7 @@
             }
         }
         this.show = show;
+        this.hide = hide;
     };
     window.popup = popup;
 })();
