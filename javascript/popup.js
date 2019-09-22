8d4867fe8d253d2858d4a9d3ab1de76159ac1ca5
tedshd <tedshd@gmail.com>
2019-09-23 01:22:26 +0800

    [dev] Update code.

--- javascript/popup.js
+++ javascript/popup.js
@@ -6,33 +6,44 @@
 
 (function() {
   var popup = function(obj) {
-    function hide(dom, dosomething) {
-      if (!dom) {
-        console.error('popup: hide function not set dom object');
-        return;
-      }
+    if (!obj.dom) {
+      console.error('popup: hide function not set dom object');
+      return;
+    }
+    var dom = obj.dom;
+
+    dom.addEventListener('click', function (e) {
+      dom.classList.add('popup_hide');
+    });
+
+    dom.querySelector('.popup_container').addEventListener('click', function (e) {
+      e.stopPropagation();
+    })
+
+    if (dom.querySelector('.popup_close')) {
+      dom.querySelector('.popup_close').addEventListener('click', function() {
+        hide(obj.dosomethingClose);
+      });
+    }
+
+    function hide(dosomething) {
       if (dosomething) {
         dosomething();
       }
       dom.classList.add('popup_hide');
+      document.body.classList.remove('popup_show');
     }
 
-    function show(dom, dosomethingShow, dosomethingClose) {
-      if (!dom) {
-        console.error('popup: show function not set dom object');
-        return;
-      }
+    function show(dosomethingShow, dosomethingClose) {
       if (dosomethingShow) {
         dosomethingShow();
       }
       dom.classList.remove('popup_hide');
+      document.body.classList.add('popup_show');
       var nodes = dom.childNodes;
-      if (dom.querySelector('popup_close')) {
-        dom.querySelector('popup_close').addEventListener('click', function() {
-          if (dosomethingClose) {
-            dosomethingClose();
-          }
-          dom.classList.add('popup_hide');
+      if (dom.querySelector('.popup_close')) {
+        dom.querySelector('.popup_close').addEventListener('click', function() {
+          hide(dosomethingClose);
         });
       }
     }
