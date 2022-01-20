0314b9d25bdbe0913ae4b7a832e4d4aafdbf6c67
Ted <tedshd@gmail.com>
2022-01-20 11:44:18 +0800

    Update popup.js

--- javascript/popup.js
+++ javascript/popup.js
@@ -4,110 +4,118 @@
  * @date    2015-07-23 17:29:42
  */
 
-(function() {
-  var popup = function(obj) {
-    var maskClose = true;
-    var pushStatus = false;
-    var back2Close = false;
-    if (!obj.dom) {
-      console.error('popup: hide function not set dom object');
-      return;
-    }
+(function () {
+      var popup = function (obj) {
+        var maskClose = true;
+        var pushStatus = false;
+        var back2Close = false;
+        var pushStateObj = {};
+        if (!obj.dom) {
+          console.error('popup: hide function not set dom object');
+          return;
+        }
 
-    var dom = obj.dom;
+        var dom = obj.dom;
 
-    if (obj.maskClose != undefined) {
-      maskClose = obj.maskClose;
-    }
+        if (obj.maskClose != undefined) {
+          maskClose = obj.maskClose;
+        }
 
-    if (obj.width != undefined) {
-      dom.querySelector('.popup_container').style.width = obj.width;
-    }
+        if (obj.width != undefined) {
+          dom.querySelector('.popup_container').style.width = obj.width;
+        }
 
-    if (obj.height != undefined) {
-      dom.querySelector('.popup_container').style.height = obj.height;
-    }
+        if (obj.height != undefined) {
+          dom.querySelector('.popup_container').style.height = obj.height;
+        }
 
-    if (obj.minWidth != undefined) {
-      dom.querySelector('.popup_container').style.minWidth = obj.minWidth;
-    }
+        if (obj.minWidth != undefined) {
+          dom.querySelector('.popup_container').style.minWidth = obj.minWidth;
+        }
 
-    if (obj.minHeight != undefined) {
-      dom.querySelector('.popup_container').style.minHeight = obj.minHeight;
-    }
+        if (obj.minHeight != undefined) {
+          dom.querySelector('.popup_container').style.minHeight = obj.minHeight;
+        }
 
-    if (obj.maxWidth != undefined) {
-      dom.querySelector('.popup_container').style.maxWidth = obj.maxWidth;
-    }
+        if (obj.maxWidth != undefined) {
+          dom.querySelector('.popup_container').style.maxWidth = obj.maxWidth;
+        }
 
-    if (obj.maxHeight != undefined) {
-      dom.querySelector('.popup_container').style.maxHeight = obj.maxHeight;
-    }
+        if (obj.maxHeight != undefined) {
+          dom.querySelector('.popup_container').style.maxHeight = obj.maxHeight;
+        }
 
-    if (maskClose) {
-      dom.addEventListener('mousedown', function (e) {
-        hide(obj.dosomethingClose);
-      });
-    }
+        if (maskClose) {
+          dom.addEventListener('mousedown', function (e) {
+            e.stopPropagation();
+            hide();
+          });
+        }
 
-    if (obj.pushStatus != undefined) {
-      pushStatus = obj.pushStatus;
-    }
+        if (obj.pushStatus != undefined) {
+          pushStatus = obj.pushStatus;
+        }
 
-    dom.querySelector('.popup_container').addEventListener('mousedown', function (e) {
-      e.stopPropagation();
-    });
-
-    if (dom.querySelector('.popup_close')) {
-      dom.querySelector('.popup_close').addEventListener('click', function() {
-        hide(obj.dosomethingClose);
-      });
-    }
-    function hide(dosomething) {
-      if (dosomething) {
-        dosomething(dom);
-      }
-      if (obj.dosomethingClose) {
-        obj.dosomethingClose(dom);
-      }
-      if (back2Close) {
-        history.back();
-      }
-      dom.classList.add('popup_hide');
-      document.body.classList.remove('popup_show');
-    }
-
-    function show(dosomethingShow, dosomethingClose) {
-      if (pushStatus) {
-        window.history.pushState({'show': 1}, '', location.href);
-        back2Close = true;
-      }
-      if (dosomethingShow) {
-        dosomethingShow(dom);
-      }
-      dom.classList.remove('popup_hide');
-      document.body.classList.add('popup_show');
-      var nodes = dom.childNodes;
-      if (dom.querySelector('.popup_close')) {
-        dom.querySelector('.popup_close').addEventListener('click', function() {
-          hide(dosomethingClose);
+        dom.querySelector('.popup_container').addEventListener('mousedown', function (e) {
+          e.stopPropagation();
         });
-      }
-    }
 
-    window.addEventListener('popstate', function() {
-      console.log('tag', back2Close)
-      if (back2Close) {
-        hide();
-        history.back();
-        return;
-      }
-      back2Close = false;
-    })
+        if (dom.querySelector('.popup_close')) {
+          dom.querySelector('.popup_close').addEventListener('click', function (e) {
+            e.stopPropagation();
+            hide();
+          });
+        }
+        function hide(dosomething) {
+          if (dosomething) {
+            dosomething(dom);
+          }
+          if (obj.dosomethingClose) {
+            obj.dosomethingClose(dom);
+          }
+          if (back2Close) {
+            back2Close = false;
+            history.back();
+          }
+          dom.classList.add('popup_hide');
+          document.body.classList.remove('popup_show');
+        }
 
-    this.show = show;
-    this.hide = hide;
-  };
-  window.popup = popup;
-})();
+        function show(dosomethingShow, dosomethingClose) {
+          if (pushStatus) {
+            if (history.state) {
+              pushStateObj = history.state;
+            }
+            pushStateObj['show'] = 1;
+            window.history.pushState(pushStateObj, '', location.href);
+            back2Close = true;
+          }
+          if (dosomethingShow) {
+            dosomethingShow(dom);
+          }
+          if (obj.dosomethingShow) {
+            obj.dosomethingShow(dom);
+          }
+          dom.classList.remove('popup_hide');
+          document.body.classList.add('popup_show');
+          var nodes = dom.childNodes;
+        }
 
+        window.addEventListener('popstate', function (e) {
+          e.preventDefault();
+          e.stopPropagation();
+          if (back2Close) {
+            back2Close = false;
+            hide();
+            delete pushStateObj['show']
+            window.history.replaceState(pushStateObj, '', this.location.href);
+            // history.back();
+            return;
+          }
+        })
+
+        this.show = show;
+        this.hide = hide;
+      };
+      window.popup = popup;
+    })();
